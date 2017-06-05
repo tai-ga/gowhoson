b44cc83ada027c85ac88fe06a0ac582f48db52f0
Masahiro Ono <masahiro.o@gmail.com>
2017-06-05 17:58:49 +0900

    Add test code for cmd/gowhoson

---
+++ cmd/gowhoson/cmd_test.go
@@ -0,0 +1,91 @@
+package gowhoson
+
+import (
+	"bytes"
+	"net"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/tai-ga/gowhoson/whoson"
+	"github.com/urfave/cli"
+)
+
+type TestEnv struct {
+	tcpconn   *net.TCPListener
+	tcpclient *whoson.Client
+	wg        *sync.WaitGroup
+}
+
+func (te *TestEnv) close() {
+	te.tcpconn.Close()
+	te.wg.Wait()
+}
+
+var te TestEnv
+
+func startTCPServer(t *testing.T) {
+	var err error
+	if te.wg == nil {
+		te.wg = new(sync.WaitGroup)
+	}
+
+	if te.tcpconn == nil {
+		addrtcp := net.TCPAddr{
+			Port: 9876,
+			IP:   net.ParseIP("localhost"),
+		}
+		te.tcpconn, err = net.ListenTCP("tcp", &addrtcp)
+		if err != nil {
+			t.Fatalf("Error %v", err)
+			return
+		}
+		te.wg.Add(1)
+		go func() {
+			defer te.wg.Done()
+			whoson.ServeTCP(te.tcpconn)
+		}()
+	}
+	return
+}
+
+func testWithServer(t *testing.T, testFuncs ...func(*cli.App)) string {
+	AppVersions = NewVersions("", "", "")
+	var buf bytes.Buffer
+	startTCPServer(t)
+
+	app := makeApp()
+	app.Writer = &buf
+	app.Metadata = map[string]interface{}{
+		"config": &whoson.ClientConfig{
+			Mode:   "tcp",
+			Server: "localhost:9876",
+		},
+	}
+
+	for _, f := range testFuncs {
+		f(app)
+	}
+	te.close()
+	return buf.String()
+}
+
+func TestCmd(t *testing.T) {
+	out := testWithServer(
+		t,
+		func(app *cli.App) {
+			app.Run([]string{"gowhoson", "client", "login", "1.1.1.1", "TESTSTRING"})
+		},
+		func(app *cli.App) {
+			app.Run([]string{"gowhoson", "client", "query", "1.1.1.1"})
+		},
+		func(app *cli.App) {
+			app.Run([]string{"gowhoson", "client", "logout", "1.1.1.1"})
+		},
+	)
+	for _, s := range []string{"+LOGIN OK", "+TESTSTRING", "+LOGOUT record deleted"} {
+		if !strings.Contains(out, s) {
+			t.Fatalf("%q should be contained in output of command: %v", s, out)
+		}
+	}
+}
